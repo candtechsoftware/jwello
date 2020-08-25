afd9e9a0d03a546691c99cdff9ed0d444b727c32
Alexmatthew Candelario <alexmatthewcandelario@AlexmatthewsMBP.home>
2020-08-24 21:03:34 -0400

    init commit

---
+++ backend/src/main/java/com/candtech/jwello/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.candtech.jwello.repository;
+
+import com.candtech.jwello.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {:w
+
+    Iterable<Project> findAllByById(Iterable<Long> iterable);
+}
