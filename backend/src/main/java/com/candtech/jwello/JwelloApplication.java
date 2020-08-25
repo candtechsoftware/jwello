3ac7c95ac586038bcaaded8c038aab0e568be12d
Alexmatthew Candelario <alexmatthewcandelario@AlexmatthewsMBP.home>
2020-08-24 20:32:55 -0400

    init commit

---
+++ backend/src/main/java/com/candtech/jwello/JwelloApplication.java
@@ -0,0 +1,13 @@
+package com.candtech.jwello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwelloApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/candtech/jwello/JwelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candtech.jwello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
