ab6455a62a6d96d84dc0439af5eed84783fdce77
Alexmatthew Candelario <alexmatthewcandelario@AlexmatthewsMBP.home>
2020-08-24 21:04:25 -0400

    fixed typo

--- backend/src/main/java/com/candtech/jwello/repository/ProjectRepository.java
+++ backend/src/main/java/com/candtech/jwello/repository/ProjectRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends CrudRepository<Project, Long> {:w
+public interface ProjectRepository extends CrudRepository<Project, Long> {
 
     Iterable<Project> findAllByById(Iterable<Long> iterable);
 }
