334534c7cb8aa9600a93a83e7922ad12ab7bd1d7
Alexmatthew Candelario <alexmatthewcandelario@AlexmatthewsMBP.home>
2020-08-27 17:52:07 -0400

    created custom exception

--- backend/src/main/java/com/candtech/jwello/services/ProjectService.java
+++ backend/src/main/java/com/candtech/jwello/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.candtech.jwello.services;
 
 import com.candtech.jwello.domain.Project;
+import com.candtech.jwello.exceptions.ProjectIdException;
 import com.candtech.jwello.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID: " + project.getProjectIdentifier().toUpperCase() + " already created");
+        }
     }
 }
