28403715fec13ff87f61df59dcf898a96299f7b1
Candtechsoftware <alex@candtechsoftware.com>
2020-09-21 20:18:57 -0400

    added get porjects func to reduxstore

--- client/src/actions/projectActions.js
+++ client/src/actions/projectActions.js
@@ -1,10 +1,26 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { bindActionCreators } from "redux";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
 
 export const createProject = (project, history) => async (dispatch) => {
 	try {
 		const res = await axios.post("http://localhost:8080/api/project", project);
 		history.push("/dashboard");
+	} catch (err) {
+		dispatch({
+			type: GET_ERRORS,
+			payload: err.response.data,
+		});
+	}
+};
+
+export const getProjects = () => async (dispatch) => {
+	try {
+		const res = await axios.get("http://localhost:8080/api/project/all");
+		dispatch({
+			type: GET_PROJECTS,
+			payload: res.data,
+		});
 	} catch (err) {
 		dispatch({
 			type: GET_ERRORS,
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,27 +1,42 @@
-import React, { Component } from 'react';
-import ProjectItem from './Projects/ProjectItem';
-import {CreateProjctButton} from './Projects/CreateProjctButton';
+import React, { Component } from "react";
+import ProjectItem from "./Projects/ProjectItem";
+import { CreateProjctButton } from "./Projects/CreateProjctButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
+
 class Dashboard extends Component {
-  render() {
-      return (
-        <div className="projects">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1 className="display-4 text-center">Projects</h1>
-                    <br />
-                    <CreateProjctButton />
-                    <br />
-                    <hr />
-                    <ProjectItem />
+	componentDidMount() {
+		this.props.getProjects();
+	}
 
-                    </div>
-            </div>
-        </div>
-    </div>
-      );
-
-  }
+	render() {
+		return (
+			<div className='projects'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-md-12'>
+							<h1 className='display-4 text-center'>Projects</h1>
+							<br />
+							<CreateProjctButton />
+							<br />
+							<hr />
+							<ProjectItem />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+	project: PropTypes.object.isRequired,
+	getProjects: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+	project: state.projects,
+});
+
+export default connect(mapStateToProps, { getProjects })(Dashboard);
